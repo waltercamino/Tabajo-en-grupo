2fb911e1220b888ee5eab1ef57b9aa5cdc4431d3
waltercamino <waltercamno@hotmail.com>
2022-06-01 18:10:16 -0300

    Comienzo del trabajo

---
+++ Trabajo practico.py
@@ -0,0 +1,15 @@
+nombre=input("Usuario: ")
+
+Lor="45 años"
+Wal="53 años"
+Max="8 años"
+
+if nombre == "Lorena" :
+        print("Edad:", Lor)
+elif nombre == "Walter" :
+       print("Edad:", Wal) 
+elif nombre == "Maxima" :
+        print("Edad:", Max)
+else: 
+        print("Edad: No Hay Datos")
+        
